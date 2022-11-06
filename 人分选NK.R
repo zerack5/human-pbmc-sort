9bee10f85aa25258d6634a5b86e7b8b1d827687e
zerack <75766273+zerack5@users.noreply.github.com>
2022-11-06 22:27:39 +0800

    Add files via upload

---
+++ 人分选NK.R
@@ -0,0 +1,139 @@
+setwd("C:/Users/a/Desktop/as分选课题/分选课题")
+library(AnnotationDbi)
+#基因注释包
+library(clusterProfiler)#富集包
+library(dplyr)
+library(ggplot2)#画图包
+library('org.Hs.eg.db')
+#biocLite('clusterProfiler')
+diff<-read.csv(file="C:/Users/a/Desktop/as分选课题/分选课题/1/CD4_res.csv")
+
+#gene.df <- bitr(diff[,1],fromType="ENSEMBL",toType="ENTREZID", OrgDb = org.Mm.eg.db）#TCGA数据框如果没有进行基因注释，那么fromType应该是Ensembl，各种ID之间可以互相转换,toType可以是一个字符串，也可以是一个向量，看自己需求                     
+#gene <- gene.df$ENTREZID
+
+
+library(biomaRt)
+listMarts()
+mart<-useMart("ensembl")
+dataset_list=as.data.frame(listDatasets(mart))
+dataset_list
+mart_oas=useMart("ensembl", "hsapiens_gene_ensembl")
+
+filter_list=as.data.frame(listFilters(mart_oas))
+tail(filter_list)
+attri_list=as.data.frame(listAttributes(mart_oas))
+attri_list
+
+list_gene=getBM(attributes = c("ensembl_gene_id","entrezgene_id","external_gene_name","description","chromosome_name"),
+                filters = "ensembl_gene_id",
+                values = diff,
+                mart = mart_oas)
+colnames(diff)[1] <- "ensembl_gene_id"
+dataf <- merge(diff,list_gene)
+
+write.csv(dataf,file = "all_diffsymbolname.csv", row.names = TRUE)
+
+
+#3、GO富集
+gene <- diff$X
+##CC表示细胞组分，MF表示分子功能，BP表示生物学过程，ALL表示同时富集三种过程，选自己需要的,我一般是做BP,MF,CC这3组再合并成一个数据框，方便后续摘取部分通路绘图。
+ego_ALL <- enrichGO(gene = gene,#我们上面定义了
+                    OrgDb=org.Hs.eg.db,
+                    keyType = "SYMBOL",
+                    ont = "ALL",#富集的GO类型
+                    pAdjustMethod = "BH",#这个不用管，一般都用的BH
+                    minGSSize = 1,
+                    pvalueCutoff = 0.01,#P值可以取0.05
+                    qvalueCutoff = 0.05,
+                    readable = TRUE)
+
+ego_CC <- enrichGO(gene = gene,
+                   OrgDb=org.Hs.eg.db,
+                   keyType = "SYMBOL",
+                   ont = "CC",
+                   pAdjustMethod = "BH",
+                   minGSSize = 1,
+                   pvalueCutoff = 0.01,
+                   qvalueCutoff = 0.05,
+                   readable = TRUE)
+
+ego_BP <- enrichGO(gene = gene,
+                   OrgDb=org.Hs.eg.db,
+                   keyType = "SYMBOL",
+                   ont = "BP",
+                   pAdjustMethod = "BH",
+                   minGSSize = 1,
+                   pvalueCutoff = 0.01,
+                   qvalueCutoff = 0.05,
+                   readable = TRUE)
+
+ego_MF <- enrichGO(gene = gene,
+                   OrgDb=org.Hs.eg.db,
+                   keyType = "SYMBOL",
+                   ont = "MF",
+                   pAdjustMethod = "BH",
+                   minGSSize = 1,
+                   pvalueCutoff = 0.01,
+                   qvalueCutoff = 0.05,
+                   readable = TRUE)
+
+#4、将结果保存到当前路径
+ego_ALL <- as.data.frame(ego_ALL)
+ego_result_BP <- as.data.frame(ego_BP)
+ego_result_CC <- as.data.frame(ego_CC)
+ego_result_MF <- as.data.frame(ego_MF)
+ego <- rbind(ego_result_BP,ego_result_CC,ego_result_MF)#或者这样也能得到ego_ALL一样的结果
+write.csv(ego_ALL,file = "ego_ALL.csv",row.names = T)
+write.csv(ego_result_BP,file = "ego_result_BP.csv",row.names = T)
+write.csv(ego_result_CC,file = "ego_result_CC.csv",row.names = T)
+write.csv(ego_result_MF,file = "ego_result_MF.csv",row.names = T)
+write.csv(ego,file = "ego.csv",row.names = T)
+
+#5、但是有时候我们富集到的通路太多了，不方便绘制图片，可以选择部分绘制，这时候跳过第4步，来到第5步
+display_number = c(11, 11, 11)#这三个数字分别代表选取的BP、CC、MF的通路条数，这个自己设置就行了
+ego_result_BP <- as.data.frame(ego_BP)[1:display_number[1], ]
+ego_result_CC <- as.data.frame(ego_CC)[1:display_number[2], ]
+ego_result_MF <- as.data.frame(ego_MF)[1:display_number[3], ]
+
+##将以上我们摘取的部分通路重新组合成数据框
+go_enrich_df <- data.frame(
+  ID=c(ego_result_BP$ID, ego_result_CC$ID, ego_result_MF$ID),                         Description=c(ego_result_BP$Description,ego_result_CC$Description,ego_result_MF$Description),
+  GeneNumber=c(ego_result_BP$Count, ego_result_CC$Count, ego_result_MF$Count),
+  type=factor(c(rep("biological process", display_number[1]), 
+                rep("cellular component", display_number[2]),
+                rep("molecular function", display_number[3])), 
+              levels=c("biological process", "cellular component","molecular function" )))
+
+##通路的名字太长了，我选取了通路的前五个单词作为通路的名字
+for(i in 1:nrow(go_enrich_df)){
+  description_splite=strsplit(go_enrich_df$Description[i],split = " ")
+  description_collapse=paste(description_splite[[1]][1:5],collapse = " ") #这里的5就是指5个单词的意思，可以自己更改
+  go_enrich_df$Description[i]=description_collapse
+  go_enrich_df$Description=gsub(pattern = "NA","",go_enrich_df$Description)
+}
+
+##开始绘制GO柱状图
+###横着的柱状图
+go_enrich_df$type_order=factor(rev(as.integer(rownames(go_enrich_df))),labels=rev(go_enrich_df$Description))#这一步是必须的，为了让柱子按顺序显示，不至于很乱
+COLS <- c("#66C3A5", "#8DA1CB", "#FD8D62")#设定颜色
+
+ggplot(data=go_enrich_df, aes(x=type_order,y=GeneNumber, fill=type)) + #横纵轴取值
+  geom_bar(stat="identity", width=0.8) + #柱状图的宽度，可以自己设置
+  scale_fill_manual(values = COLS) + ###颜色
+  coord_flip() + ##这一步是让柱状图横过来，不加的话柱状图是竖着的
+  xlab("GO term") + 
+  ylab("Gene_Number") + 
+  labs(title = "The Most Enriched GO Terms of CD4")+
+  theme_bw()
+
+###竖着的柱状图 
+go_enrich_df$type_order=factor(rev(as.integer(rownames(go_enrich_df))),labels=rev(go_enrich_df$Description))
+COLS <- c("#66C3A5", "#8DA1CB", "#FD8D62")
+ggplot(data=go_enrich_df, aes(x=type_order,y=GeneNumber, fill=type)) + 
+  geom_bar(stat="identity", width=0.8) + 
+  scale_fill_manual(values = COLS) + 
+  theme_bw() + 
+  xlab("GO term") + 
+  ylab("Num of Genes") + 
+  labs(title = "The Most Enriched GO Terms")+ 
+  theme(axis.text.x=element_text(face = "bold", color="gray50",angle = 70,vjust = 1, hjust = 1 ))#angle是坐标轴字体倾斜的角度，可以自己设置
